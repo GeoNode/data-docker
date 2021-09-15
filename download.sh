72cb6c5058c1522e867d0e3b4e13b3c309dea5cb
Luca Pasquali <luca@bucaniere.us>
2021-09-15 11:59:25 +0200

    fixed datadir URL with versionless artifact file name

--- download.sh
+++ download.sh
@@ -11,10 +11,8 @@ echo "Archive temporary directory is $2"
 GEOSERVER_VERSION=$1
 TEMP_DOWNLOADED=$2 
 
-GEOSERVER_SNAPSHOT_VERSION="${GEOSERVER_VERSION::-2}-SNAPSHOT"
-
 echo "GeoServer Data Directory is going to be downloaded"
-artifact_url="https://artifacts.geonode.org/geoserver/$GEOSERVER_VERSION/geonode-geoserver-ext-web-app-$GEOSERVER_SNAPSHOT_VERSION-data.zip"
+artifact_url="https://artifacts.geonode.org/geoserver/$GEOSERVER_VERSION/geonode-geoserver-ext-web-app-data.zip"
 echo "Downloading: $artifact_url"
-curl  -k -L "$artifact_url" --output data-$GEOSERVER_VERSION.zip && unzip -x -d ${TEMP_DOWNLOADED} data-$GEOSERVER_VERSION.zip
+curl  -k -L "$artifact_url" --output data.zip && unzip -x -d ${TEMP_DOWNLOADED} data.zip
 echo "GeoServer Data Directory download has been completed"
