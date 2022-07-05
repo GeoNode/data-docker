689a8a8eb363c337055a9c8452a06e02a417e037
afabiani <alessio.fabiani@geosolutionsgroup.com>
2022-07-05 13:22:22 +0200

    - Fix artifact url

--- download.sh
+++ download.sh
@@ -12,7 +12,7 @@ GEOSERVER_VERSION=$1
 TEMP_DOWNLOADED=$2 
 
 echo "GeoServer Data Directory is going to be downloaded"
-artifact_url="https://artifacts.geonode.org/geoserver/$GEOSERVER_VERSION/geonode-geoserver-ext-web-app-data.zip"
+artifact_url="https://artifacts.geonode.org/geoserver/releases/$GEOSERVER_VERSION/geonode-geoserver-ext-web-app-$GEOSERVER_VERSION-data.zip"
 echo "Downloading: $artifact_url"
-curl  -k -L -O "$artifact_url" && unzip -x -d ${TEMP_DOWNLOADED} data-$GEOSERVER_VERSION.zip
+curl  -k -L "$artifact_url" --output data.zip && unzip -x -d ${TEMP_DOWNLOADED} data.zip
 echo "GeoServer Data Directory download has been completed"
